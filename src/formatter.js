5060b77e01788bc2a2250abbffc956332f861745
KaptainMicila <micillotto@gmail.com>
2020-11-01 12:10:33 +0100

    VERSION 0.9.2
    I won't try even continuing the formatter.
    Screw javascript and VSCode's formatter.

--- src/formatter.js
+++
@@ -1,9 +0,0 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-vscode.languages.registerDocumentFormattingEditProvider('zscript', {
-    provideDocumentFormattingEdits(document) {
-		
-    },
-});
