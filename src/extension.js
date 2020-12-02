9f27cc28e74d430dfb65f235f2bd71cb031858a6
KaptainMicila <micillotto@gmail.com>
2020-12-02 18:47:48 +0100

    VERSION 0.9.5b4

--- src/extension.js
+++
@@ -1,9 +0,0 @@
-const vscode = require('vscode');
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {}
-
-// eslint-disable-next-line no-undef
-module.exports = { activate };
