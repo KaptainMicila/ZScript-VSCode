683e915739a1cbfc90d3678d129fb0593cca3359
KaptainMicila <micillotto@gmail.com>
2021-02-07 18:42:33 +0100

    START
    Added Typescript insertion point

---
+++ src/extension.ts
@@ -0,0 +1,24 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand(
+        'gzdoom-zscript.helloWorld',
+        () => {
+            // The code you place here will be executed every time your command is executed
+
+            // Display a message box to the user
+            vscode.window.showInformationMessage(
+                'Hello World from Gzdoom ZScript!'
+            );
+        }
+    );
+
+    context.subscriptions.push(disposable);
+}
