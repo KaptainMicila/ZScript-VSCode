f3dcb867c64f6951d43128e2bbe76316e3f4b5a4
KaptainMicila <micillotto@gmail.com>
2021-11-07 10:34:10 +0100

    Web extension now does the same things the normal extension does

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,9 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import {activate as NormalActivation} from "../extension";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    vscode.window.showWarningMessage("The features for this version are still WIP! Please, be patient.");
+    /*
+     * This activates the same things the "base" extension does.
+     * If it's gonna break something, we'll think about it later.
+     */
+    NormalActivation(context);
 }
