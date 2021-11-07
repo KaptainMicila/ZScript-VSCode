9e222bbfddaefd788f1b554fc6e636c227367fa9
KaptainMicila <micillotto@gmail.com>
2021-11-07 16:44:29 +0100

    Added ""useless"" comments, because everytime I read my own code I forgot why I wrote it like that

--- src/Services/ZScriptErrorService.ts
+++ src/Services/ZScriptErrorService.ts
@@ -41,6 +41,7 @@ export default class ZScriptErrorService {
             }
 
             if (char === "}") {
+                // If a "}" is found but there's no "{" before it, an Error is added to the list.
                 if (bracketsBuffer.pop() === undefined) {
                     zscriptErrors.push(
                         new ZScriptError(
@@ -53,6 +54,7 @@ export default class ZScriptErrorService {
             }
         }
 
+        // For each "{" that doesn't have a "}" an Error is added to the list.
         for (const position of bracketsBuffer) {
             zscriptErrors.push(
                 new ZScriptError(
@@ -63,6 +65,7 @@ export default class ZScriptErrorService {
             );
         }
 
+        // Updates the diagnostics once it ends searching for unopened/unclosed contextes.
         this.updateDiagnostics(
             document.uri,
             bracketErrorCollection,
